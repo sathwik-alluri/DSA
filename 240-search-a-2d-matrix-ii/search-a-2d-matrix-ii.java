352bc6a2ee83e8c7aea0ac2e7997f70e01927adb
sathwik varma alluri <109074035+sathwik-alluri@users.noreply.github.com>
2026-08-22 18:52:11 +0530

    Time: 3 ms (87.86%) | Memory: 48.1 MB (84.38%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) 
     {
         int n=matrix.length;
-        int m=matrix[0].length;
+        int m=matrix[0].length; //TC: O(log mn)
 
         int l=0;
         int r=m-1;
