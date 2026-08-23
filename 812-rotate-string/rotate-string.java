30fe946fbd78fe90d2295e1bb8b2aad09704fcdf
sathwik varma alluri <109074035+sathwik-alluri@users.noreply.github.com>
2026-08-23 20:25:27 +0530

    Time: 1 ms (97.82%) | Memory: 43.8 MB (6.45%) - LeetSync

---
+++ 812-rotate-string/rotate-string.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean rotateString(String s, String goal) 
+    {
+        // String t=goal;                   //This is my approach. But it will not work for the test cases where duplicate caharacters exist. BEcause we are iverriding the position value in the array in later part.
+        // if(s.length() != t.length())
+        //     return false;
+
+        // int arr[]=new int[26];
+        // int n=s.length();
+
+        // char firstEle=s.charAt(0);
+        // int firstElePos=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     arr[goal.charAt(i) - 'a'] = i+1;
+        //     if(goal.charAt(i) == firstEle)
+        //         firstElePos=i+1;
+        // }
+
+        // int diff=firstElePos - (0+1);
+        // for(int i=0;i<n;i++)
+        // {   
+        //     int temp = arr[s.charAt(i) - 'a'] - (i+1);
+        //     if(temp < 0)
+        //     {
+        //         temp = temp + n;
+        //     }
+        //     if(temp != diff)
+        //         return false;
+        // }
+        // return true;
+
+
+        if(s.length() != goal.length())
+            return false;
+        int n=s.length();
+        for(int i=0;i<n;i++)
+        {
+            String rotated = s.substring(i, n) + s.substring(0,i);
+
+            if(rotated.equals(goal))
+                return true;
+        }
+        return false;
+    }
+}
