ca3ec65fccf4d7af7716894d2a8ea6e1f39c97d4
sathwik varma alluri <109074035+sathwik-alluri@users.noreply.github.com>
2026-08-23 16:54:51 +0530

    Time: 2 ms (99.75%) | Memory: 43.7 MB (26.57%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,32 +1,42 @@
 class Solution {
     public String removeOuterParentheses(String s) 
     {
-        int n=s.length();
-        String ans="";
-        int c=0;
-        for(int i=0;i<n;i++)
+        // int n=s.length();
+        // String ans="";
+        // int c=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(s.charAt(i) == '(')
+        //         c++;
+        //     else
+        //         c--;
+
+        //     if(c==0 || (c==1 && s.charAt(i) == '('))
+        //         continue;
+            
+        //     ans+=s.charAt(i);
+            
+        // }
+        // return ans;
+
+
+        StringBuilder result = new StringBuilder();  
+        int level = 0;  
+        for (char ch : s.toCharArray()) 
         {
-            if(s.charAt(i) == '(')
+            if (ch == '(') 
             {
-                c++;
+                if (level > 0)        //We have to skip the insertion for 1st level
+                    result.append(ch);
+                level++; 
+            } 
+            else if (ch == ')') 
+            {
+                level--;  
+                if (level > 0)  // If we're inside a primitive, adding ')' to result
+                    result.append(ch);
             }
-            else
-                c--;
-
-            // if(c==1 && s.charAt(i) == '(')
-            //     continue;
-
-            if(c==0 || (c==1 && s.charAt(i) == '('))
-                continue;
-
-            // else if(c==0 && s.charAt(i) == ')'))
-            // {
-            //     continue;
-            // }
-            
-            ans+=s.charAt(i);
-            
         }
-        return ans;
+        return result.toString();
     }
 }
