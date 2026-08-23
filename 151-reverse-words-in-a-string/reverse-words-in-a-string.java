63e407ecfae290fa36214556562b151c628fe6dc
sathwik varma alluri <109074035+sathwik-alluri@users.noreply.github.com>
2026-08-23 17:42:31 +0530

    Time: 6 ms (80.69%) | Memory: 43.5 MB (99.59%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,20 +1,66 @@
 class Solution {
     public String reverseWords(String s) 
     {
-        String arr[]=s.trim().split("\\s+");
+        // String arr[]=s.trim().split("\\s+");
+        // int n=arr.length;
+        // int l=0;
+        // int r=n-1;
+        // while(l<=r)
+        // {
+        //     String t=arr[l];
+        //     arr[l]=arr[r];
+        //     arr[r]=t;
+        //     l++;
+        //     r--;
+        // }
+        // String Sentence = String.join(" ", arr);
+        // return Sentence;
 
-        int n=arr.length;
-        int l=0;
-        int r=n-1;
-        while(l<=r)
+        s=s.trim();
+        int n=s.length();
+        StringBuilder str=new StringBuilder();
+        StringBuilder word=new StringBuilder();
+        int i=n-1;
+        while(i>=0)
         {
-            String t=arr[l];
-            arr[l]=arr[r];
-            arr[r]=t;
-            l++;
-            r--;
+            if(s.charAt(i) !=  ' ')
+            {
+                word.append(s.charAt(i));
+                i--;
+            }
+            else
+            {
+                if(str.length() == 0)
+                {
+                    str.append(word.reverse());
+                    word.setLength(0);
+                }
+                else   
+                {
+                    str.append(" ");
+                    str.append(word.reverse());
+                    word.setLength(0);
+                }
+                while(i>=0 && s.charAt(i) == ' ')
+                {
+                    i--;
+                    continue;
+                }
+            }
         }
-        String Sentence = String.join(" ", arr);
-        return Sentence;
+        if(word.length() !=0)
+        {
+            if(str.length() == 0)
+            {
+                str.append(word.reverse());
+                word.setLength(0);
+            }
+            else   
+            {
+                str.append(" ");
+                str.append(word.reverse());
+            }
+        }
+        return str.toString();
     }
 }
