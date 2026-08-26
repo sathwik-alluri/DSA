ed5af0f37d00e5f44a5a36d578c6fdc13d32a86a
sathwik varma alluri <109074035+sathwik-alluri@users.noreply.github.com>
2026-08-26 21:34:24 +0530

    Time: 12 ms (47.42%) | Memory: 47.6 MB (44.17%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,32 +1,55 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) 
     {
+        // int n=s.length();
+        // int l=0;
+        // int r=0;
+        // int arr[]=new int[256];
+        // int maxLen=0;
+        // while(r<n)
+        // {
+        //     if(arr[s.charAt(r)] == 0)
+        //     {
+        //         arr[s.charAt(r)] += 1;
+        //         maxLen=Math.max(maxLen, r-l+1);
+        //         r++;
+        //     }
+        //     else
+        //     {
+        //         while(l<r && s.charAt(l) != s.charAt(r))
+        //         {
+        //             arr[s.charAt(l)] -= 1;
+        //              l++;
+        //         }
+        //         arr[s.charAt(l)] -= 1;
+        //         l++;
+
+        //         arr[s.charAt(r)] += 1;
+        //         r++;
+        //     }
+        // }
+        // return maxLen;
+
         int n=s.length();
         int l=0;
         int r=0;
         int arr[]=new int[256];
+        for(int i=0;i<256;i++)
+            arr[i]=-1;
         int maxLen=0;
         while(r<n)
         {
-            if(arr[s.charAt(r)] == 0)
+            if(arr[s.charAt(r)] == -1 || arr[s.charAt(r)] < l)
             {
-                arr[s.charAt(r)] += 1;
-                maxLen=Math.max(maxLen, r-l+1);
-                r++;
+                arr[s.charAt(r)] = r;
             }
             else
             {
-                while(l<r && s.charAt(l) != s.charAt(r))
-                {
-                    arr[s.charAt(l)] -= 1;
-                     l++;
-                }
-                arr[s.charAt(l)] -= 1;
-                l++;
-                
-                arr[s.charAt(r)] += 1;
-                r++;
+                l=arr[s.charAt(r)] + 1;
+                arr[s.charAt(r)] = r;
             }
+            maxLen=Math.max(maxLen, r-l+1);
+            r++;
         }
         return maxLen;
     }
