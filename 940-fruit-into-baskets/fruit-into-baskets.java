e9a0710faaefcb9f2607dfdf36f4c32811a786bf
sathwik varma alluri <109074035+sathwik-alluri@users.noreply.github.com>
2026-08-27 21:50:01 +0530

    Time: 52 ms (83.84%) | Memory: 68.4 MB (89.19%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,6 +1,34 @@
 class Solution {
     public int totalFruit(int[] fruits) 
     {
+        // int n=fruits.length;
+        // int l=0;
+        // int r=0;
+        // int maxLen=0;
+        // HashMap<Integer, Integer> hm=new HashMap<>();
+        // while(r<n)
+        // {
+        //     hm.put(fruits[r], hm.getOrDefault(fruits[r], 0)+1);
+
+        //     if(hm.size() <= 2)
+        //         maxLen=Math.max(maxLen, r-l+1);
+            
+        //     else
+        //     {
+        //         while(hm.size() > 2)
+        //         {
+        //             hm.put(fruits[l], hm.get(fruits[l])-1);
+        //             if(hm.get(fruits[l]) == 0)
+        //                 hm.remove(fruits[l]);
+        //             l++;
+        //         }
+        //         maxLen=Math.max(maxLen, r-l+1);
+        //     }
+        //     r++;
+        // }
+        // return maxLen;
+
+
         int n=fruits.length;
         int l=0;
         int r=0;
@@ -15,14 +43,10 @@ class Solution {
             
             else
             {
-                while(hm.size() > 2)
-                {
-                    hm.put(fruits[l], hm.get(fruits[l])-1);
-                    if(hm.get(fruits[l]) == 0)
-                        hm.remove(fruits[l]);
-                    l++;
-                }
-                maxLen=Math.max(maxLen, r-l+1);
+                hm.put(fruits[l], hm.get(fruits[l])-1);
+                if(hm.get(fruits[l]) == 0)
+                    hm.remove(fruits[l]);
+                l++;
             }
             r++;
         }
