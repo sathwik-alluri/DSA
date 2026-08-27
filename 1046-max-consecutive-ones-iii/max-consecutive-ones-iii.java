8e0de5633bba107877006fe6b7709cf769c9fb3f
sathwik varma alluri <109074035+sathwik-alluri@users.noreply.github.com>
2026-08-27 09:01:47 +0530

    Time: 4 ms (44.27%) | Memory: 51.7 MB (96.86%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,36 +1,62 @@
 class Solution {
     public int longestOnes(int[] nums, int k) 
     {
+        // int n=nums.length;
+        // int l=0;
+        // int r=0;
+        // int c=0;
+        // int maxLen=0;
+        // PriorityQueue<Integer> ZeroIndexes = new PriorityQueue<>();
+        // while(r<n)
+        // {
+        //     if(nums[r] == 1)
+        //     {
+        //         maxLen=Math.max(maxLen, r-l+1);
+        //         r++;
+        //     }
+        //     else    //Ele is 0
+        //     {
+        //         c+=1;
+        //         ZeroIndexes.add(r);
+        //         if(c<=k)
+        //         {
+        //             maxLen=Math.max(maxLen, r-l+1);
+        //             r++;
+        //         }
+        //         else
+        //         {
+        //             l=ZeroIndexes.poll()+1;
+        //             c-=1;
+        //             maxLen=Math.max(maxLen, r-l+1);
+        //             r++;
+        //         }
+        //     }
+        // }
+        // return maxLen;
+
+
         int n=nums.length;
         int l=0;
         int r=0;
-        int c=0;
+        int z=0;
         int maxLen=0;
-        PriorityQueue<Integer> ZeroIndexes = new PriorityQueue<>();
         while(r<n)
         {
-            if(nums[r] == 1)
+            if(nums[r] == 0)
+            {
+                z++;
+            }
+            if(z > k)
+            {
+                if(nums[l] == 0)
+                    z=z-1;
+                l++;
+            }
+            if(z <= k)
             {
                 maxLen=Math.max(maxLen, r-l+1);
-                r++;
             }
-            else    //Ele is 0
-            {
-                c+=1;
-                ZeroIndexes.add(r);
-                if(c<=k)
-                {
-                    maxLen=Math.max(maxLen, r-l+1);
-                    r++;
-                }
-                else
-                {
-                    l=ZeroIndexes.poll()+1;
-                    c-=1;
-                    maxLen=Math.max(maxLen, r-l+1);
-                    r++;
-                }
-            }
+            r++;
         }
         return maxLen;
     }
