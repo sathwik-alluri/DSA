25ba095be23d45c064cb5d578000f1697c7184a9
sathwik varma alluri <109074035+sathwik-alluri@users.noreply.github.com>
2026-08-28 09:33:53 +0530

    Time: 10 ms (55.74%) | Memory: 46.1 MB (64.95%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,7 +1,39 @@
 class Solution {
     public int characterReplacement(String s, int k) 
     {
-        int n=s.length();
+        // int n=s.length();   //TC: O(n + n*26)
+        // int l=0;
+        // int r=0;
+        // int maxLen=0;
+        // int hm[]=new int[26];
+        // int maxfreq=0;
+        // while(r<n)
+        // {
+        //     hm[s.charAt(r) - 'A']++;
+        //     maxfreq=Math.max(maxfreq, hm[s.charAt(r) - 'A']);
+
+        //     //int changesReq=(r-l+1)-maxFreq;
+        //     while((r-l+1)-maxfreq > k)
+        //     {
+        //         hm[s.charAt(l) - 'A']--;
+        //         l++;
+        //         maxfreq=0;
+        //         for(int i=0;i<26;i++)   //Updating new Max frequency
+        //         {
+        //             maxfreq=Math.max(maxfreq, hm[i]);
+        //         }
+        //     }
+
+        //     if((r-l+1)-maxfreq <= k)
+        //     {
+        //         maxLen=Math.max(maxLen, r-l+1);
+        //     }
+        //     r++;
+        // }
+        // return maxLen;
+
+
+        int n=s.length();   //TC: O(n)
         int l=0;
         int r=0;
         int maxLen=0;
@@ -12,16 +44,11 @@ class Solution {
             hm[s.charAt(r) - 'A']++;
             maxfreq=Math.max(maxfreq, hm[s.charAt(r) - 'A']);
 
-            //int changesReq=(r-l+1)-maxFreq;
-            while((r-l+1)-maxfreq > k)
+            if((r-l+1)-maxfreq > k)
             {
                 hm[s.charAt(l) - 'A']--;
                 l++;
                 maxfreq=0;
-                for(int i=0;i<26;i++)   //Updating new Max frequency
-                {
-                    maxfreq=Math.max(maxfreq, hm[i]);
-                }
             }
 
             if((r-l+1)-maxfreq <= k)
