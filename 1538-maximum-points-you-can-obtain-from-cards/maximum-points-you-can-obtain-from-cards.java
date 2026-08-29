7ad90ae1263079c086c6e7c9742510387bff3a3b
sathwik varma alluri <109074035+sathwik-alluri@users.noreply.github.com>
2026-08-29 13:12:24 +0530

    Time: 3 ms (34.00%) | Memory: 61.5 MB (87.30%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
@@ -1,23 +1,58 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) 
     {
-        int n=cardPoints.length;
-        int maxSum=0;
-        int lsum=0;
-        int rsum=0;
-        for(int i=0;i<k;i++)
+        int n = cardPoints.length;
+
+    int totalSum = 0;
+
+    for(int x : cardPoints)
+        totalSum += x;
+
+    int windowSize = n - k;
+
+    int l = 0;
+    int r = 0;
+    int windowSum = 0;
+    int minSum = Integer.MAX_VALUE;
+
+    while(r < n)
+    {
+        windowSum += cardPoints[r];
+
+        // Keep window size = n-k
+        if(r - l + 1 > windowSize)
         {
-            lsum+=cardPoints[i];
+            windowSum -= cardPoints[l];
+            l++;
         }
-        maxSum=lsum;
-        int rind=n-1;
-        for(int i=k-1;i>=0;i--)
+
+        if(r - l + 1 == windowSize)
         {
-            lsum-=cardPoints[i];
-            rsum+=cardPoints[rind];
-            rind--;
-            maxSum=Math.max(maxSum, lsum + rsum);
+            minSum = Math.min(minSum, windowSum);
         }
-        return maxSum;
+
+        r++;
+    }
+
+    return totalSum - minSum;
+
+        // int n=cardPoints.length;
+        // int maxSum=0;
+        // int lsum=0;
+        // int rsum=0;
+        // for(int i=0;i<k;i++)
+        // {
+        //     lsum+=cardPoints[i];
+        // }
+        // maxSum=lsum;
+        // int rind=n-1;
+        // for(int i=k-1;i>=0;i--)
+        // {
+        //     lsum-=cardPoints[i];
+        //     rsum+=cardPoints[rind];
+        //     rind--;
+        //     maxSum=Math.max(maxSum, lsum + rsum);
+        // }
+        // return maxSum;
     }
 }
