a8d77f2bf3b087f2ad56c6307696b89eb61d74bd
sathwik varma alluri <109074035+sathwik-alluri@users.noreply.github.com>
2026-08-29 13:15:03 +0530

    Time: 2 ms (99.70%) | Memory: 61.8 MB (53.59%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
@@ -1,58 +1,55 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) 
     {
-        int n = cardPoints.length;
+        /* Instead of directly finding the maximum sum of k cards from the front or back, consider the cards that remain. The remaining cards will always form a continuous subarray of size n-k. So, find the minimum sum window of size n-k using sliding window, then return totalSum - minWindowSum. TC: O(n), SC: O(1).  */
 
-    int totalSum = 0;
+        // int n = cardPoints.length;
+        // int totalSum = 0;
 
-    for(int x : cardPoints)
-        totalSum += x;
+        // for(int x : cardPoints)
+        //     totalSum += x;
 
-    int windowSize = n - k;
+        // int windowSize = n - k;
 
-    int l = 0;
-    int r = 0;
-    int windowSum = 0;
-    int minSum = Integer.MAX_VALUE;
+        // int l = 0;
+        // int r = 0;
+        // int windowSum = 0;
+        // int minSum = Integer.MAX_VALUE;
 
-    while(r < n)
-    {
-        windowSum += cardPoints[r];
-
-        // Keep window size = n-k
-        if(r - l + 1 > windowSize)
-        {
-            windowSum -= cardPoints[l];
-            l++;
-        }
-
-        if(r - l + 1 == windowSize)
-        {
-            minSum = Math.min(minSum, windowSum);
-        }
-
-        r++;
-    }
-
-    return totalSum - minSum;
-
-        // int n=cardPoints.length;
-        // int maxSum=0;
-        // int lsum=0;
-        // int rsum=0;
-        // for(int i=0;i<k;i++)
+        // while(r < n)
         // {
-        //     lsum+=cardPoints[i];
+        //     windowSum += cardPoints[r];
+        //     // Keep window size = n-k
+        //     if(r - l + 1 > windowSize)
+        //     {
+        //         windowSum -= cardPoints[l];
+        //         l++;
+        //     }
+        //     if(r - l + 1 == windowSize)
+        //     {
+        //         minSum = Math.min(minSum, windowSum);
+        //     }
+        //     r++;
         // }
-        // maxSum=lsum;
-        // int rind=n-1;
-        // for(int i=k-1;i>=0;i--)
-        // {
-        //     lsum-=cardPoints[i];
-        //     rsum+=cardPoints[rind];
-        //     rind--;
-        //     maxSum=Math.max(maxSum, lsum + rsum);
-        // }
-        // return maxSum;
+        // return totalSum - minSum;
+
+        int n=cardPoints.length;
+        int maxSum=0;
+        int lsum=0;
+        int rsum=0;
+        for(int i=0;i<k;i++)
+        {
+            lsum+=cardPoints[i];
+        }
+        maxSum=lsum;
+        int rind=n-1;
+        for(int i=k-1;i>=0;i--)
+        {
+            lsum-=cardPoints[i];
+            rsum+=cardPoints[rind];
+            rind--;
+            maxSum=Math.max(maxSum, lsum + rsum);
+        }
+        return maxSum;
     }
 }
