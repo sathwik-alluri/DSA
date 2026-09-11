950344ed7ecf549c90adaa6435306a9bac10d648
sathwik varma alluri <109074035+sathwik-alluri@users.noreply.github.com>
2026-09-11 18:46:43 +0530

    Time: 6 ms (4.19%) | Memory: 48.2 MB (32.39%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,32 +1,51 @@
 class Solution {
     public int trap(int[] height) 
     {
+        // int n = height.length;
+        // int[] leftMax = new int[n];
+        // int[] rightMax = new int[n];
+
+        // // Build prefix maximum
+        // leftMax[0] = height[0];
+        // for (int i = 1; i < n; i++) {
+        //     leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        // }
+        // // Build suffix maximum
+        // rightMax[n - 1] = height[n - 1];
+        // for (int i = n - 2; i >= 0; i--) {
+        //     rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        // }
+
+        // // Calculate trapped water
+        // int water = 0;
+        // for (int i = 0; i < n; i++) {
+        //     water += Math.min(leftMax[i], rightMax[i]) - height[i];
+        // }
+        // return water;
+
+
         int n = height.length;
+        Stack<Integer> st=new Stack<>();
+        int water=0;
+        for(int i=0;i<n;i++)
+        {
+            while(!st.isEmpty() && height[st.peek()] < height[i])
+            {
+                int current=height[st.pop()];
+                if(!st.isEmpty())
+                {
+                    int left=st.peek();
+                    int right=i;
 
-        int[] leftMax = new int[n];
-        int[] rightMax = new int[n];
+                    int width=(right-left)-1;
 
-        // Build prefix maximum
-        leftMax[0] = height[0];
+                    int w=Math.min(height[left], height[right])-current;
 
-        for (int i = 1; i < n; i++) {
-            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+                    water+=width*w;
+                }
+            }
+            st.push(i);
         }
-
-        // Build suffix maximum
-        rightMax[n - 1] = height[n - 1];
-
-        for (int i = n - 2; i >= 0; i--) {
-            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
-        }
-
-        // Calculate trapped water
-        int water = 0;
-
-        for (int i = 0; i < n; i++) {
-            water += Math.min(leftMax[i], rightMax[i]) - height[i];
-        }
-
         return water;
     }
 }
