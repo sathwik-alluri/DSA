5b8cfdff339fded44450c3b462ec487ab9ca0cc3
sathwik varma alluri <109074035+sathwik-alluri@users.noreply.github.com>
2026-09-12 11:31:24 +0530

    Time: 5 ms (64.88%) | Memory: 47.3 MB (12.08%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,34 +1,64 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) 
     {
+        // int n=asteroids.length;
+        // Stack<Integer> st=new Stack<>();
+
+        // for(int i=0;i<n;i++)
+        // {
+        //     int curr=asteroids[i];
+        //     int same=0;
+        //     while(st.isEmpty() == false)
+        //     {
+        //         if((st.peek() > 0 && curr<0) && -1*curr >= st.peek())
+        //         {
+        //             if(-1*curr == st.peek())
+        //             {
+        //                 st.pop();
+        //                 same=1;
+        //                 break;
+        //             }
+        //             st.pop();
+        //         }
+        //         else
+        //             break;
+        //     }
+        //     if(same == 1)
+        //         continue;
+        //     else if(st.isEmpty() || (st.peek() > 0 && curr>0) || (st.peek() < 0 && curr<0) || (st.peek() < 0 && curr>0))
+        //         st.push(curr);
+        // }
+
+        // int ans[]=new int[st.size()];
+        // int i=st.size()-1;
+        // while(st.isEmpty() == false)
+        // {
+        //     ans[i]=st.pop();
+        //     i--;
+        // }
+        // return ans;
+
+
         int n=asteroids.length;
         Stack<Integer> st=new Stack<>();
-
         for(int i=0;i<n;i++)
         {
             int curr=asteroids[i];
-            int same=0;
-            while(st.isEmpty() == false)
-            {
-                if((st.peek() > 0 && curr<0) && -1*curr >= st.peek())
-                {
-                    if(-1*curr == st.peek())
-                    {
-                        st.pop();
-                        same=1;
-                        break;
-                    }
-                    st.pop();
-                }
-                else
-                    break;
-            }
-            if(same == 1)
-                continue;
-            else if(st.isEmpty() || (st.peek() > 0 && curr>0) || (st.peek() < 0 && curr<0) || (st.peek() < 0 && curr>0))
+            if(curr > 0)
                 st.push(curr);
-        }
+            
+            else
+            {
+                while(!st.isEmpty() && st.peek()>0 && st.peek()<Math.abs(curr))
+                    st.pop();
+                
+                if(!st.isEmpty() && st.peek()>0 && st.peek() == Math.abs(curr))
+                    st.pop();
 
+                else if(st.isEmpty() || st.peek() < 0)
+                    st.push(curr);
+            }      
+        }
         int ans[]=new int[st.size()];
         int i=st.size()-1;
         while(st.isEmpty() == false)
