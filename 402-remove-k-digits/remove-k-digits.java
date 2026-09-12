92795a0924795eab829b3cf5d09730bd4a4a0e0e
sathwik varma alluri <109074035+sathwik-alluri@users.noreply.github.com>
2026-09-12 19:23:55 +0530

    Time: 90 ms (5.85%) | Memory: 48.5 MB (6.23%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String removeKdigits(String num, int k) 
+    {
+        int n=num.length();
+        Stack<Character> st=new Stack<>();
+        if(n == k)
+            return "0";
+        for(int i=0;i<n;i++)
+        {
+            while(k>0 && st.isEmpty()==false && st.peek()-'0'>num.charAt(i)-'0')
+            {
+                st.pop();
+                k--;
+            }
+            st.push(num.charAt(i));
+        }
+        while(k>0)     //We many encounter case, we are not able to pop any elents. Then we need to remove last k numbers from ans.
+        //Ex: 123456
+        {
+            k--;
+            st.pop();
+        }
+
+        String ans="";
+        while(st.isEmpty() == false)
+        {
+            ans += st.pop();
+        }
+
+        ans=new StringBuilder(ans).reverse().toString();
+        ans=ans.replaceFirst("^0+", "");
+
+        if(ans == "")
+            return "0";
+        return ans;
+    }
+}
