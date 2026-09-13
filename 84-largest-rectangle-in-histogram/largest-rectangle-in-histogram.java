131ff459f7e4ddf84906f552877b7ff6cc50cc57
sathwik varma alluri <109074035+sathwik-alluri@users.noreply.github.com>
2026-09-13 11:30:55 +0530

    Time: 63 ms (70.10%) | Memory: 78.3 MB (44.27%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,55 +1,90 @@
 class Solution {
     public int largestRectangleArea(int[] heights) 
     {
+        // int n=heights.length;     //TC: O(2n)+O(2n)+O(n) == O(5n)
+        // int nse[]=nextSmallerElement(heights);
+        // int pse[]=previousSmallerElement(heights);
+        // int ans=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     int w=nse[i]-pse[i]-1;
+        //     int area=w*heights[i];
+
+        //     ans=Math.max(ans, area);
+        // }
+        // return ans;
+
+
         int n=heights.length;
-        int nse[]=nextSmallerElement(heights);
-        int pse[]=previousSmallerElement(heights);
+        Stack<Integer> st=new Stack<>();
         int ans=0;
         for(int i=0;i<n;i++)
         {
-            int w=nse[i]-pse[i]-1;
-            int area=w*heights[i];
+            while(st.isEmpty()==false && heights[st.peek()] > heights[i])
+            {
+                int curr=heights[st.pop()];
+                int prevSmaller=-1;
+                if(st.isEmpty() == false)
+                    prevSmaller=st.peek();
+                int nextSmaller=i;
 
+                int width=nextSmaller-prevSmaller-1;
+                int area=width*curr;
+                ans=Math.max(ans, area);
+            }
+            st.push(i);
+        }
+
+        while(st.isEmpty()==false)
+        {
+            int curr=heights[st.pop()];
+            int prevSmaller=-1;
+            if(st.isEmpty() == false)
+                prevSmaller=st.peek();
+            int nextSmaller=n;
+
+            int width=nextSmaller-prevSmaller-1;
+            int area=width*curr;
             ans=Math.max(ans, area);
         }
         return ans;
     }
 
-    public int[] previousSmallerElement(int[] arr) 
-    {
-        int n=arr.length;
-        int[] pse=new int[n];
-        Arrays.fill(pse, -1);
-        Stack<Integer> st=new Stack<>();
-        for(int i=0;i<n;i++)
-        {
-            while(st.isEmpty()==false && arr[st.peek()] >= arr[i])
-            {
-                st.pop();
-            }
-            if(!st.isEmpty())
-                pse[i]=st.peek();
-            st.push(i);
-        }
-        return pse;
-    }
+    // public int[] previousSmallerElement(int[] arr) 
+    // {
+    //     int n=arr.length;
+    //     int[] pse=new int[n];
+    //     Arrays.fill(pse, -1);
+    //     Stack<Integer> st=new Stack<>();
+    //     for(int i=0;i<n;i++)
+    //     {
+    //         while(st.isEmpty()==false && arr[st.peek()] >= arr[i])
+    //         {
+    //             st.pop();
+    //         }
+    //         if(!st.isEmpty())
+    //             pse[i]=st.peek();
+    //         st.push(i);
+    //     }
+    //     return pse;
+    // }
 
-    public int[] nextSmallerElement(int[] arr) 
-    {
-        int n=arr.length;
-        int[] nse=new int[n];
-        Arrays.fill(nse, n);
-        Stack<Integer> st=new Stack<>();
-        for(int i=n-1;i>=0;i--)
-        {
-            while(st.isEmpty()==false && arr[st.peek()] >= arr[i])
-            {
-                st.pop();
-            }
-            if(!st.isEmpty())
-                nse[i]=st.peek();
-            st.push(i);
-        }
-        return nse;
-    }
+    // public int[] nextSmallerElement(int[] arr) 
+    // {
+    //     int n=arr.length;
+    //     int[] nse=new int[n];
+    //     Arrays.fill(nse, n);
+    //     Stack<Integer> st=new Stack<>();
+    //     for(int i=n-1;i>=0;i--)
+    //     {
+    //         while(st.isEmpty()==false && arr[st.peek()] >= arr[i])
+    //         {
+    //             st.pop();
+    //         }
+    //         if(!st.isEmpty())
+    //             nse[i]=st.peek();
+    //         st.push(i);
+    //     }
+    //     return nse;
+    // }
 }
