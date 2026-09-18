35e97b6fab2635cc890e34896b20a39658adc974
sathwik varma alluri <109074035+sathwik-alluri@users.noreply.github.com>
2026-09-18 17:52:06 +0530

    Time: 1 ms (100.00%) | Memory: 46.3 MB (76.99%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -11,64 +11,97 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) 
     {
-        if(length(l2) > length(l1))
-        {
-            ListNode temp = l1;
-            l1 = l2;
-            l2 = temp;
-        }
+        ListNode t1=l1;
+        ListNode t2=l2;
 
-        ListNode temp1 = l1;
-        ListNode temp2 = l2;
-        ListNode prev = null;
+        ListNode dummy=new ListNode(-1);
+        ListNode dum=dummy;
+
         int carry=0;
 
-        while(temp2!=null)
+        while(t1!=null || t2!=null)
         {
-            temp1.val=temp1.val + temp2.val + carry;
+            int sum=carry;
+            if(t1!=null)
+                sum+=t1.val;
+            if(t2!=null)
+                sum+=t2.val;
 
-            if(temp1.val >= 10)
-            {
-                int t=temp1.val;
-                temp1.val = temp1.val%10;
-                carry=t/10;
-            }
-            else
-                carry=0;
+            ListNode newNode = new ListNode(sum % 10);
+            dum.next=newNode;
+            dum=newNode;
+            carry = sum/10;
 
-            prev=temp1;
-            temp2=temp2.next;
-            temp1=temp1.next;
+            if(t1!=null)
+                t1=t1.next;
+            if(t2!=null)
+                t2=t2.next;
         }
+
+        if(carry > 0)
+        {
+            dum.next=new ListNode(carry);
+        }
+        return dummy.next;
+
+        // if(length(l2) > length(l1))
+        // {
+        //     ListNode temp = l1;
+        //     l1 = l2;
+        //     l2 = temp;
+        // }
+
+        // ListNode temp1 = l1;
+        // ListNode temp2 = l2;
+        // ListNode prev = null;
+        // int carry=0;
+
+        // while(temp2!=null)
+        // {
+        //     temp1.val=temp1.val + temp2.val + carry;
+
+        //     if(temp1.val >= 10)
+        //     {
+        //         int t=temp1.val;
+        //         temp1.val = temp1.val%10;
+        //         carry=t/10;
+        //     }
+        //     else
+        //         carry=0;
+
+        //     prev=temp1;
+        //     temp2=temp2.next;
+        //     temp1=temp1.next;
+        // }
         
-        if(carry > 0)
-        {
-            while(temp1!=null)
-            {
-                temp1.val=temp1.val + carry;
+        // if(carry > 0)
+        // {
+        //     while(temp1!=null)
+        //     {
+        //         temp1.val=temp1.val + carry;
 
-                if(temp1.val < 10)
-                {
-                    carry=0;
-                    break;
-                }
-                else    // if(temp1.val >= 10)
-                {
-                    int t=temp1.val;
-                    temp1.val = temp1.val%10;
-                    carry=t/10;
-                }
-                prev=temp1;
-                temp1=temp1.next;
-            }
-        }
+        //         if(temp1.val < 10)
+        //         {
+        //             carry=0;
+        //             break;
+        //         }
+        //         else    // if(temp1.val >= 10)
+        //         {
+        //             int t=temp1.val;
+        //             temp1.val = temp1.val%10;
+        //             carry=t/10;
+        //         }
+        //         prev=temp1;
+        //         temp1=temp1.next;
+        //     }
+        // }
 
-        if(carry > 0)
-        {
-            ListNode newNode = new ListNode(1);
-            prev.next=newNode;
-        }
-        return l1;
+        // if(carry > 0)
+        // {
+        //     ListNode newNode = new ListNode(1);
+        //     prev.next=newNode;
+        // }
+        // return l1;        
     }
 
     public int length(ListNode head)
